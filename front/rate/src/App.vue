dd527e9663d0fdcf881d6fdf4b7cffb01718a6e3
y.pavlov <lenvonder12vorh@gmail.com>
2021-12-08 14:18:19 +0300

    Hardcode width for rate

--- front/rate/src/App.vue
+++ front/rate/src/App.vue
@@ -42,4 +42,42 @@ export default {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
 }
+
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 600px) {
+
+    #app {
+        width: 300px;
+    }
+}
+
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {
+
+    #app {
+        width: 600px;
+    }
+}
+
+/* Medium devices (landscape tablets, 768px and up) */
+@media only screen and (min-width: 768px) {
+
+    #app {
+        width: 768px;
+    }
+}
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px) {
+    #app {
+        width: 992px;
+    }
+}
+
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {
+    #app {
+        width: 1200px;
+    }
+}
 </style>
