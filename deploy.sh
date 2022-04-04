4246db4287c55d062f394b8766f5a5b76c3b9b17
y.pavlov <lenvonder12vorh@gmail.com>
2022-04-04 16:30:16 +0300

    Update github-actions-develop.yml

--- deploy.sh
+++ deploy.sh
@@ -61,8 +61,8 @@ if [[ "$deploySSH" == "ssh" ]];  then
           cr.yandex
 
   ssh $url docker pull cr.yandex/crpg11k469bhc8lch9gm/overnight/widget:$tag
-  ssh $url docker-compose -f /root/ovn/docker-compose.yaml up -d --no-deps widget
-  ssh $url docker logs ovn-widget -f
+  ssh $url docker-compose -f /root/common/docker-compose.yaml up -d --no-deps widget
+  ssh $url docker logs widget -f
 fi
 
 
